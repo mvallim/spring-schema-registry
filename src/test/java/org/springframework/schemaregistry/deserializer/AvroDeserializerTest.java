f9e052f67c10c6f114a9f16f34a6fedca57ed0c0
Marcos Tischer Vallim <tischer@gmail.com>
2026-05-06 00:41:49 -0300

    ci: update workflow

--- src/test/java/org/springframework/schemaregistry/deserializer/AvroDeserializerTest.java
+++ src/test/java/org/springframework/schemaregistry/deserializer/AvroDeserializerTest.java
@@ -26,19 +26,19 @@ import java.io.IOException;
 import org.apache.avro.specific.SpecificRecord;
 import org.apache.kafka.common.errors.SerializationException;
 import org.apache.kafka.common.header.internals.RecordHeaders;
-import org.junit.Test;
+import org.junit.jupiter.api.Test;
 import org.springframework.schemaregistry.serializer.AvroSerializer;
 
 import example.avro.User;
 
-public class AvroDeserializerTest {
+class AvroDeserializerTest {
 
   private final AvroSerializer avroSerializer = new AvroSerializer();
 
   private final AvroDeserializer avroDeserializer = new AvroDeserializer();
 
   @Test
-  public void testSuccessDeserializer() throws IOException {
+  void testSuccessDeserializer() throws IOException {
     final RecordHeaders headers = new RecordHeaders();
     final byte[] bs = avroSerializer.serialize("bogus", headers, createAvroRecord());
     final SpecificRecord specificRecord = avroDeserializer.deserialize("bogus", headers, bs);
@@ -46,7 +46,7 @@ public class AvroDeserializerTest {
   }
 
   @Test
-  public void testSuccessDeserializerNullData() throws IOException {
+  void testSuccessDeserializerNullData() throws IOException {
     final RecordHeaders headers = new RecordHeaders();
     final byte[] bs = avroSerializer.serialize("bogus", headers, null);
     final SpecificRecord specificRecord = avroDeserializer.deserialize("bogus", headers, bs);
@@ -54,13 +54,13 @@ public class AvroDeserializerTest {
   }
 
   @Test
-  public void testSuccessDeserializerRiseSerializationException() throws IOException {
+  void testSuccessDeserializerRiseSerializationException() throws IOException {
     final Throwable throwable = catchThrowable(() -> avroDeserializer.deserialize("bogus", null, new byte[] { 0 }));
     assertThat(throwable, instanceOf(SerializationException.class));
   }
 
   @Test
-  public void testSuccessDeserializerRiseUnsupportedOperationException() throws IOException {
+  void testSuccessDeserializerRiseUnsupportedOperationException() throws IOException {
     final Throwable throwable = catchThrowable(() -> avroDeserializer.deserialize("bogus", new byte[] { 0 }));
     assertThat(throwable, instanceOf(UnsupportedOperationException.class));
   }
